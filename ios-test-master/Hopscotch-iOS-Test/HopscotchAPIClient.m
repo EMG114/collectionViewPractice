821813a7319880a82281ddc43fe710214bb626ea
Sara <saraahmad1114@yahoo.com>
2017-01-12 16:05:21 -0500

    changes

--- ios-test-master/Hopscotch-iOS-Test/HopscotchAPIClient.m
+++ ios-test-master/Hopscotch-iOS-Test/HopscotchAPIClient.m
@@ -16,23 +16,17 @@ NSString *const HOPSCOTCH_API_URL = @"https://hopscotch-ios-test.herokuapp.com/p
 
 +(void)getProjectsWithCompletion:(void (^)(NSArray *))completionBlock{
 
-     // create a string to turn into a URL
     NSString *urlString = @"https://hopscotch-ios-test.herokuapp.com/projects";
     
     NSURL *nsurlString = [NSURL URLWithString:urlString];
-    //conversion of urlString into NSURL String
     
     NSURLSession *thisSession = [NSURLSession sharedSession];
-    //creating the session
     
-    // create NSURLSessionDataTask using thisSession
     NSURLSessionDataTask *thisTask = [thisSession dataTaskWithURL:nsurlString completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
     {
         NSDictionary *jsonResponseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        //initial json is wrapped as an NSDictionary
         
         NSArray *projectsArray = [jsonResponseDictionary valueForKey:@"projects"];
-        //only need the value of projects, which is an NSArray
                                           
         completionBlock(projectsArray);
     }];
